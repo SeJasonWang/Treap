2caf0f77541a6750f6b665d1f6b1c6068b967db4
Sichen Wang <sichen.wang@qq.com>
2019-05-18 15:38:02 +1000

    Initial Commit

---
+++ Treap/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Treap
+//
+//  Created by Jason on 18/5/19.
+//  Copyright © 2019 UTAS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
